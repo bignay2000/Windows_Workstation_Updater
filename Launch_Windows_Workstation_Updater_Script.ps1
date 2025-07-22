2b44bfdf032f36a5b054fc439f67ee9f41a16961
Ryan Naylor <bignay2000@hotmail.com>
2025-07-22 18:40:10 -0400

    Fix Launch

--- Launch_Windows_Workstation_Updater_Script.ps1
+++ Launch_Windows_Workstation_Updater_Script.ps1
@@ -9,12 +9,10 @@ Function ScriptDirectory {
 $scriptDir = ScriptDirectory
 
 if (Test-Path -Path "$scriptDir/logs"){
-
     Write-Output "$scriptDir/logs already exists"
 } else {
     Write-Output "Creating $scriptDir/logs directory"
     mkdir "$scriptDir/logs"
 }
 
-
-& Script_Functions.ps1 2>&1 | tee-object -Append "$scriptDir/logs/$StartTime-Windows_Workstation_Updater-$Env:ComputerName.log"
+& .\Script_Functions.ps1 2>&1 | tee-object -Append "$scriptDir/logs/$StartTime-Windows_Workstation_Updater-$Env:ComputerName.log"

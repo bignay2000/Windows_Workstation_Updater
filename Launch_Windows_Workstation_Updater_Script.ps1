d0154554851b32cf9ff355d9e37017d55eba4c6f
Ryan Naylor <bignay2000@hotmail.com>
2025-07-28 21:42:51 -0400

    Update Launch_Windows_Workstation_Updater_Script.ps1

--- Launch_Windows_Workstation_Updater_Script.ps1
+++ Launch_Windows_Workstation_Updater_Script.ps1
@@ -1,7 +1,8 @@
-#Requires -Version 7.5.1
+#Ryan Naylor
+#Requires -Version 7.5.2
 $WarningPreference = "Stop";
 $ErrorActionPreference = "Stop";
-$StartTime = $((Get-Date).ToString('MM-dd-yyyy_HH-mm-ss') )
+$StartTime = $((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss') )
 Function ScriptDirectory {
     $Invocation = (Get-Variable MyInvocation -Scope 1).Value
     Split-Path $Invocation.MyCommand.Path
@@ -9,10 +10,12 @@ Function ScriptDirectory {
 $scriptDir = ScriptDirectory
 
 if (Test-Path -Path "$scriptDir/logs"){
+
     Write-Output "$scriptDir/logs already exists"
 } else {
     Write-Output "Creating $scriptDir/logs directory"
     mkdir "$scriptDir/logs"
 }
+
 
 & .\Script_Functions.ps1 2>&1 | tee-object -Append "$scriptDir/logs/$StartTime-Windows_Workstation_Updater-$Env:ComputerName.log"
